8eb37e18152f1e51b28e88a4958c034cf121745c
Rohit Chauhan <rohitchauhan6232@gmail.com>
2026-03-16 11:07:35 +0530

    added helmet, morgon, and compression for security and better debuging

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
+require('dotenv').config({ quiet: true });
 const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
-
+const helmet = require('helmet');
+const morgan = require('morgan');
+const compression = require('compression');
 const globalRoute = require('./routes/globalRoute');
 const { authMiddleware } = require('./services/globalModule');
 const limiter = require('./services/limiter.js');
 
+app.set('trust proxy', 1);
+
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(compression());
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json({ limit: '50mb' }));
-
 app.use(cors());
-
 app.use('/static', express.static(path.join(__dirname, 'Uploads')));
 
-app.use(limiter);  // rate limiting
-app.use(authMiddleware);  // authentication of user
+app.use(limiter);       
+app.use(authMiddleware); 
 app.use('/api', globalRoute);
 
 const PORT = process.env.SERVER_PORT || 6232;
-
 app.listen(PORT, () => {
     console.log(`Server is live and listening on http://localhost:${PORT}/api`);
 });
